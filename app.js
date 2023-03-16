4877919c329edb9c718e4a91fd367680f78df161
ChaosCow58 <sryan228@g.fhsdschools.org>
2023-03-15 21:05:58 -0500

    Finish adding the rules

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const sqlite3 = require('sqlite3').verbose();
+let table = 'CREATE TABLE WORDS (ID NUMBER)'
 
 const app = express();
 
@@ -11,9 +12,12 @@ app.get('/', (_req, res) => {
     res.sendFile('index.html');
 });
 
+app.get('/dictionary', (_req, res) => {
+    res.sendFile('public/dictionary.html', {root: __dirname});
+});
+
 app.listen(5000, () => {
     console.log('Server started!');
-    
 });
 
 let db = new sqlite3.Database('public/words.db', (err) => {
@@ -23,6 +27,18 @@ let db = new sqlite3.Database('public/words.db', (err) => {
 
     console.log('Contected to the SQlite database.')
 });
+
+// db.serialize(() => {
+//     db.each(`SELECT WordId as id, 
+//         Word as word
+//     FROM words`, (err, row) => {
+//         if (err) {
+//             console.error(err.message);
+//         }
+
+//         console.log(row.id + "\t" + row.name);
+//     });
+// });
 
 db.close((err) => {
     if (err) {
