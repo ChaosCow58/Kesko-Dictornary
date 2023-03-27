eb79fb7dd95b668adbfbaf55b22cce5d982be7cc
ChaosCow58 <sryan228@g.fhsdschools.org>
2023-03-26 22:33:24 -0500

    Almost replaced xml with json

--- public/AWords.js
+++ public/AWords.js
@@ -1,24 +1,61 @@
-function myFunction(xml) {
-    var xmlDoc = xml.responseXML;
-    var entries = xmlDoc.getElementsByTagName("entry");
-    var divBody = "";
-    for (var i = 0; i < entries.length; i++) {
-        var word = entries[i].getElementsByTagName("word")[0].textContent;
-        var headWord = entries[i].getElementsByTagName("headWord")[0].textContent;
-        var type = entries[i].getElementsByTagName("type")[0].textContent;
-        var pronounce = entries[i].getElementsByTagName("pronounce")[0].textContent;
-        var definition = entries[i].getElementsByTagName("definition")[0].innerHTML;
-        var definitionHTML = definition.replace(/(\r\n|\n|\r|&#13;)/gm, "<br>");
-        divBody += "\n        <div class='wordBox'>\n            <h2>".concat(word, "</h2>\n            <p><span class='speech'>Headword:</span> ").concat(headWord, "</p>\n            <p><span class='speech'>Type:</span> ").concat(type, "</p>\n            <p><span class='speech'>Pronunciation:</span> <span class='pronunciation'>/").concat(pronounce, "/</span></p>\n\n            <div class='definition'>\n                <h3>Definition</h3>\n                <p>").concat(definitionHTML, "</p>\n            </div>\n        </div>\n        ");
+let xhr = new XMLHttpRequest();
+xhr.open('GET', "/databases/words.json");
+
+
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        let words = JSON.parse(xhr.responseText);
+
+        let verbWords = words.filter(function(word) {
+            return word.group === "verb";
+        });
+
+        let AWords = words.filter(function(word) {
+            return word.group === "A";
+        });
+
+       localStorage.setItem("verbData", JSON.stringify(verbWords));
+       localStorage.setItem("AData", JSON.stringify(AWords));
+
+        let placeholder = document.querySelector("#data-output");
+        let out = "";
+
+        for (let word of AWords) {
+
+            out += `
+            <div class='wordBox'>
+                <h2>${word.word}</h2>
+                <p><span class='speech'>Headword:</span> ${word.headword}</p>
+                <p><span class='speech'>Type:</span> ${word.type}</p>
+                <p><span class='speech'>Pronunciation:</span> <span class='pronunciation'>/${word.pronounce}/</span></p>
+
+                <div class='definition'>
+                    <h3>Definition</h3>
+                    <p>${word.definition}</p>
+                </div>
+            </div>
+            
+            `;
+        }
+
+        for (let word of verbWords) {
+            out += `
+            <div class='wordBox'>
+                <h2>${word.word}</h2>
+                <p><span class='speech'>Headword:</span> ${word.headword}</p>
+                <p><span class='speech'>Type:</span> ${word.type}</p>
+                <p><span class='speech'>Pronunciation:</span> <span class='pronunciation'>/${word.pronounce}/</span></p>
+
+                <div class='definition'>
+                    <h3>Definition</h3>
+                    <p>${word.definition}</p>
+                </div>
+            </div>
+            `;
+        }
+        placeholder.innerHTML = out;
     }
-    var main = document.getElementById("main");
-    main.innerHTML += divBody;
-}
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
-    }
+
+    
 };
-xhttp.open("GET", "/databases/AWords.xml", true);
-xhttp.send();
+xhr.send();
